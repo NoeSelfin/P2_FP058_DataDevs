b3eb43ad1bfe08b72742434842f82452d726cfbb
ainhoammm <ainhoamonje1@gmail.com>
2023-10-31 00:34:01 +0100

    Añadida carpeta: Test y clase ejemplo para testear

---
+++ Tests/modelo/ListaClientesTest.java
@@ -0,0 +1,28 @@
+package modelo;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ListaClientesTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void printList() {
+    }
+
+    @Test
+    void printListEstandar() {
+    }
+
+    @Test
+    void printListPremium() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
